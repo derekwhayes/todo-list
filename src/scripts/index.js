24852f6ac0bafa002577bcb2c0662193ea64340d
Derek Hayes <derek2099@gmail.com>
2024-10-18 18:32:50 -0500

    Add event listeners for various buttons

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,5 @@
 import "../styles/style.css";
-import tasks from "./tasks.js";
+import {tasks} from "./tasks.js";
 
 class Task {
     constructor(title, description, dueDate, priority, notes) {
@@ -11,14 +11,45 @@ class Task {
     }
 }
 
-// sample data
-const sampleTask = new Task('Sample Task', 'Sample Description', '10-31-2024', 'low', 'Get it done.');
-const sampleTask2 = new Task('Second Task', 'This is a second task', '11-03-2024', 'medium', 'For real.');
-const sampleTask3 = new Task('Third Sample', 'This again?', '12-12-2024', 'low', "That's enough, I think.");
+const createSampleData = (() => {
+    // sample data
+    const sampleTask = new Task('Sample Task', 'Sample Description', '10-31-2024', 'low', 'Get it done.');
+    const sampleTask2 = new Task('Second Task', 'This is a second task', '11-03-2024', 'medium', 'For real.');
+    const sampleTask3 = new Task('Third Sample', 'This again?', '12-12-2024', 'low', "That's enough, I think.");
 
-const taskData = [];
-taskData.push(sampleTask);
-taskData.push(sampleTask2);
-taskData.push(sampleTask3);
+    const taskData = [];
+    taskData.push(sampleTask);
+    taskData.push(sampleTask2);
+    taskData.push(sampleTask3);
 
-tasks.addTaskData(taskData);
+    return {
+        taskData
+    }
+})();
+
+const tasksInstance = tasks();
+const taskListItems = tasksInstance.addTaskData(createSampleData.taskData);
+
+
+
+const fabBtnPressed = () => {
+    console.log('fabBtn pressed');
+}
+
+tasksInstance.fabDiv.addEventListener('click', fabBtnPressed);
+
+taskListItems.forEach((task) => {
+    task.addEventListener('click', (e) => {
+        console.log(`${e.target} clicked!`);
+    });
+});
+
+const homeBtn = document.querySelector('h1');
+homeBtn.addEventListener('click', () => {
+    console.log('Home clicked');
+});
+
+const menuBtn = document.querySelector('.menuBtn');
+menuBtn.addEventListener('click', () => {
+    console.log('Menu clicked');
+});
